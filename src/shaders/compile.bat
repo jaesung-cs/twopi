896a657edb6d2d6e0d39926394287aa7c37c778e
Jaesung <jaesung.cs@gmail.com>
2021-04-04 10:06:05 +0900

    deleted unused files

--- src/shaders/compile.bat
+++
@@ -1,4 +0,0 @@
-echo %~dp0
-if not exist "../../bin/shaders" mkdir "../../bin/shaders"
-C:/VulkanSDK/1.2.162.0/Bin32/glslc.exe %~dp0%1.vert -o %~dp0../../bin/shaders/%1.vert.spv
-C:/VulkanSDK/1.2.162.0/Bin32/glslc.exe %~dp0%1.frag -o %~dp0../../bin/shaders/%1.frag.spv
