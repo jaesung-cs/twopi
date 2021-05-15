aa10f2d7e480fbc287b00abfa04d59a5a9c02826
Jaesung <jaesung.cs@gmail.com>
2021-05-15 22:35:23 +0900

    make bigger surface

--- src/twopi/vkl/primitive/vkl_surface.cc
+++ src/twopi/vkl/primitive/vkl_surface.cc
@@ -6,35 +6,29 @@ namespace vkl
 {
 Surface::Surface()
 {
-  position_buffer_.push_back(-1.f);
-  position_buffer_.push_back(-1.f);
-  position_buffer_.push_back(0.f);
-
-  position_buffer_.push_back(1.f);
-  position_buffer_.push_back(-1.f);
-  position_buffer_.push_back(0.f);
-
-  position_buffer_.push_back(-1.f);
-  position_buffer_.push_back(1.f);
-  position_buffer_.push_back(0.f);
-
-  position_buffer_.push_back(1.f);
-  position_buffer_.push_back(1.f);
-  position_buffer_.push_back(0.f);
-
-  for (int i = 0; i < 4; i++)
+  for (int i = 0; i < 3; i++)
   {
-    vx_buffer_.push_back(2.f);
-    vx_buffer_.push_back(0.f);
-    vx_buffer_.push_back(2.f);
+    for (int j = 0; j < 3; j++)
+    {
+      position_buffer_.push_back((j - 1) * 2.f);
+      position_buffer_.push_back(0.f);
+      position_buffer_.push_back((i - 1) * 2.f);
 
-    vy_buffer_.push_back(0.f);
-    vy_buffer_.push_back(2.f);
-    vy_buffer_.push_back(2.f);
+      vx_buffer_.push_back(2.f);
+      vx_buffer_.push_back(2.f);
+      vx_buffer_.push_back(0.f);
+
+      vy_buffer_.push_back(0.f);
+      vy_buffer_.push_back(2.f);
+      vy_buffer_.push_back(2.f);
+    }
   }
 
   index_buffer_ = {
-    0, 1, 2, 3
+    0, 1, 3, 4,
+    1, 2, 4, 5,
+    3, 4, 6, 7,
+    4, 5, 7, 8,
   };
 }
 
